2d4d9d0ba12e8ec562ba0e482d36965421a0b5d4
super-cheetos <tkruddnjs1@naver.com>
2023-03-18 11:52:22 +0900

    프로젝트 생성

---
+++ src/main/java/io/supercheetos/blogsearchservice/BlogSearchServiceApplication.java
@@ -0,0 +1,13 @@
+package io.supercheetos.blogsearchservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogSearchServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogSearchServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/supercheetos/blogsearchservice/BlogSearchServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.supercheetos.blogsearchservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogSearchServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

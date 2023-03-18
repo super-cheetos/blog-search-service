bb291f87f924081d63cb5fcff0f8062a0b825551
super-cheetos <tkruddnjs1@naver.com>
2023-03-18 20:24:35 +0900

    Keyword count index 추가

--- src/main/java/io/supercheetos/blogsearchservice/keyword/Keyword.java
+++ src/main/java/io/supercheetos/blogsearchservice/keyword/Keyword.java
@@ -1,23 +1,17 @@
 package io.supercheetos.blogsearchservice.keyword;
 
-import java.time.Instant;
-
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.EntityListeners;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import java.time.Instant;
 
 @Entity
-@Table(name = "keyword")
+@Table(name = "keyword", indexes = @Index(name = "idx_keyword_count", columnList = "count DESC"))
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @EntityListeners(AuditingEntityListener.class)
